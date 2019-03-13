44f194775c9d7ac441bdd3ad01482f41c5c6ad58
Imon Ovbude <imonovbude@gmail.com>
2019-03-13 13:43:29 -0700

    test if control buttons are present

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,23 @@
 // Test away!
+import React from 'react';
+import { render } from 'react-testing-library';
+
+import Controls from './Controls';
+
+describe('<Controls />', () => {
+    it('should provide a closed button', () => {
+        const { getByTestId } = render(<Controls />)
+
+        const lockButton = getByTestId('lockButton');
+
+        expect(lockButton).toBeDefined();
+    })
+
+    it('should provide a gate button', () => {
+        const { getByTestId } = render(<Controls />);
+
+        const gateButton = getByTestId('gateButton');
+
+        expect(gateButton).toBeDefined();
+    })
+})
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,15 @@
 // Test away
+import React from 'react';
+import { render } from 'react-testing-library';
+import renderer from 'react-test-renderer';
+import 'react-testing-library/cleanup-after-each';
+
+import Dashboard from './Dashboard';
+
+describe('<Dashboard />', () => {
+    it.skip('should show the controls', () => {
+        const tree = renderer.create(<Dashboard />)
+
+        expect(tree.toJSON()).toMatchSnapshot()
+    })
+})
