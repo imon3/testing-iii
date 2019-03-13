3741bde08b5414c6a24caff38037067d57ba5406
Imon Ovbude <imonovbude@gmail.com>
2019-03-13 15:47:06 -0700

    test if default open gate is open

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -3,26 +3,25 @@ import React from 'react';
 import { render, fireEvent } from 'react-testing-library';
 import renderer from 'react-test-renderer';
 import 'react-testing-library/cleanup-after-each';
+import 'jest-dom/extend-expect';
 
 import Dashboard from './Dashboard';
 
 describe('<Dashboard />', () => {
-    it.skip('should show the controls', () => {
+    it('should show the controls', () => {
         const tree = renderer.create(<Dashboard />)
 
         expect(tree.toJSON()).toMatchSnapshot()
-    })
-})
+    });
+});
 
 describe('<Dashboard', () => {
     it('should show button text changes when clicked ', () => {
         const { getByText } = render(<Dashboard />);
 
+        const closeButton = getByText(/close gate/i);
+        fireEvent.click(closeButton);
 
-        const closeButton = getByText(/close gate/i)
-        fireEvent.click(closeButton)
-
-        expect(closeButton).not.toBe(/close gate/i)
-
-    })
-})
+        expect(getByText(/open gate/i)).toBeDefined();
+    });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,16 @@
 // Test away!
+import React from 'react';
+import { render } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each'
+import 'jest-dom/extend-expect';
+
+import Display from './Display';
+
+describe('<Display />', () => {
+    it('should display if gate is open', () => {
+        const { getByText } = render(<Display />);
+        const openGateDisplay = getByText(/open/i);
+
+        expect(openGateDisplay);
+    });
+});
